52b1ff541b3a31b0b01cd1a8011047004f2f15c2
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-04-15 22:36:41 +0400

    update

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 import numpy as np
+
+
 st.write(""" 
 
-# simple Stock Price App
+## ***Dumbledore Team*** 
 
 Shown are the stock closing price and volume of Google 
 
