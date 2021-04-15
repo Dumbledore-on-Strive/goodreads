5377a3942fdc3bd56b23c04fb3b7f61836aa4fbf
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-04-15 22:21:01 +0400

    update

--- app.py
+++ app.py
@@ -11,4 +11,8 @@ Shown are the stock closing price and volume of Google
 tickerSymbol = 'Google'
 tickerData = yf.ticker(tickerSymbol)
 
-tickerDf = tickerData
+tickerDf = tickerData.history(period='id', start='2010-5-31', end='2020-4-15')
+
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
