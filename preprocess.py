26ee0cd495ecb5d7f9f9b0612ed687ec622ec108
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-04-15 13:55:52 +0400

    update

--- preprocess.py
+++ preprocess.py
@@ -3,83 +3,63 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-df = pd.read_csv("testing.csv")
-df.columns
-df = df.drop(['Unnamed: 0', 'place', 'url'], axis=1)
-df = df.dropna()
-df['num_pages'] = pd.to_numeric(df['num_pages'], downcast='integer')
-df.head()
 
-# Step 1: find min(avg_rating)
-minValue = df['avg_rating'].min()
-print('Minimum rating: ', minValue)
+def preprocess():
 
-# Step2: find max(avg_rating)
-maxValue = df['avg_rating'].max()
-print('Maximum rating: ', maxValue)
+    df = pd.read_csv("goodreads_1000_books_list.csv")
+    df = df.drop(['Unnamed: 0', 'place', 'url'], axis=1)
+    df['num_pages'] = pd.to_numeric(df['num_pages'], downcast='integer')
 
-# Step3 min max norm for average rating
+    def minmax_norm(data_column_name):
+        x = data_column_name
+        mean_norm_ratings = 1+((x-x.min())/(x.max()-x.min()))*9
+        return mean_norm_ratings
 
+    df['minmax_norm_ratings'] = minmax_norm(
+        df['avg_rating']).round(decimals=2)  # little problem
 
-def minmax_norm(data_column_name):
-    x = data_column_name
-    mean_norm_ratings = 1+((x-x.min())/(x.max()-x.min()))*9
-    return mean_norm_ratings
+    df.head()
 
+    # # Mean normalization
+    #Step1 : average (avg_rating)
+    df_mean = df[["avg_rating"]].mean()
+    df_mean
 
-df['minmax_norm_ratings'] = minmax_norm(
-    df['avg_rating']).round(decimals=2)  # little problem
+    def mean_norm(data_column_name):
+        x = data_column_name
+        mean_norm_ratings = 1+((x-x.mean())/(x.max()-x.min()))*9
+        return mean_norm_ratings
 
-df.head()
+    a = mean_norm(df["avg_rating"])
+    a.to_frame()
 
-# # Mean normalization
-#Step1 : average (avg_rating)
-df_mean = df[["avg_rating"]].mean()
-df_mean
+    df['mean_norm_ratings'] = mean_norm(df['avg_rating']).round(decimals=2)
+    df.head()
+    cols = df.columns.tolist()
+    cols = cols[:8]+cols[-2:]+cols[8:-2]
+    df = df[cols]
+    df
 
+    df.to_csv('preprocess_data.csv')
 
-def mean_norm(data_column_name):
-    x = data_column_name
-    mean_norm_ratings = 1+((x-x.mean())/(x.max()-x.min()))*9
-    return mean_norm_ratings
+    # #                                                                            Analyse
 
+    anay_goup = df.groupby("original_publish_year")[
+        'minmax_norm_ratings'].mean().round(decimals=2)
 
-a = mean_norm(df["avg_rating"])
-a.to_frame()
+    # In[63]:
 
-df['mean_norm_ratings'] = mean_norm(df['avg_rating']).round(decimals=2)
-df.head()
-cols = df.columns.tolist()
-cols = cols[:8]+cols[-2:]+cols[8:-2]
-df = df[cols]
-df
+    anay_goup.to_frame()
 
-df.to_csv('preprocess_data.csv')
+    # In[61]:
 
-# #                                                                            Analyse
+    anay_goup.to_frame()
 
-anay_goup = df.groupby("original_publish_year")[
-    'minmax_norm_ratings'].mean().round(decimals=2)
+    # #                                         The Best Book Author
 
+    def get_book_author(name, df):
+        f = df[df.loc[:, 'author'] == name]
+        m = f['minmax_norm_ratings'].max()
+        return m
 
-# In[63]:
-
-
-anay_goup.to_frame()
-
-
-# In[61]:
-
-
-anay_goup.to_frame()
-
-
-# #                                         The Best Book Author
-
-def get_book_author(name, df):
-    f = df[df.loc[:, 'author'] == name]
-    m = f['minmax_norm_ratings'].max()
-    return m
-
-
-get_book_author('Cassandra Clare', df)
+    get_book_author('Cassandra Clare', df)
