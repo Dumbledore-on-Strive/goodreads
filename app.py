e5d9d8ae6e18595f91b4472ab4d81c7ca3ee1ebf
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-04-15 22:17:53 +0400

    update

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
+st.write(""" 
+
+# simple Stock Price App
+
+Shown are the stock closing price and volume of Google 
+
+""")
+tickerSymbol = 'Google'
+tickerData = yf.ticker(tickerSymbol)
+
+tickerDf = tickerData
